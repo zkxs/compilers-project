87cb941c0b5950f85902a94a0a3517e88a8ceda4
Michael Ripley <zkxs00@gmail.com>
2016-08-29 01:46:42 -0500

    Removed 72-character restriction

--- src/net/michaelripley/pascalcompiler/Lister.scala
+++ src/net/michaelripley/pascalcompiler/Lister.scala
@@ -6,12 +6,7 @@ object Lister {
   def main(args: Array[String]): Unit = {
     Source.fromFile("test.pas").getLines().zipWithIndex.foreach {
       case (line, index) => {
-        if (line.length > 71) {
-          println(s"ERROR: Line $index is longer than 72 characters")
-          System.exit(1)
-        } else {
-          println(f"${index + 1}%5d: $line")
-        }
+        println(f"${index + 1}%5d: $line")
       }
     }
   }
