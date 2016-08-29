55adcb09df90b0e9109676bec48e8825ab736343
Michael Ripley <zkxs00@gmail.com>
2016-08-29 02:26:21 -0500

    Tokens can now be duplicated

--- src/net/michaelripley/pascalcompiler/Token.scala
+++ src/net/michaelripley/pascalcompiler/Token.scala
@@ -1,12 +1,22 @@
 package net.michaelripley.pascalcompiler
 
-class Token(val lexeme: String, val token: String, attributeString: String) {
-  val attribute = {
-    if (attributeString.isEmpty()) {
-      None
-    } else {
-      Option(attributeString)
-    }
+class Token(val lexeme: String, val token: String, val attribute: Option[String]) {
+  
+  def this(lexeme: String, token: String, attributeString: String) = {
+    this(lexeme, token, 
+      if (attributeString.isEmpty()) {
+        None
+      } else {
+        Option(attributeString)
+      }
+    )
+  }
+  
+  /**
+   * Duplicate this token but using a different lexeme
+   */
+  def duplicate(lexeme: String) = {
+    new Token(lexeme, token, attribute)
   }
   
   override def equals(o: Any): Boolean = {
