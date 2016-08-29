da2842df2bebb2deddc746b01b4d6d2f3e329d6a
Michael Ripley <zkxs00@gmail.com>
2016-08-29 01:46:54 -0500

    Made a line in the test file longer that 72 characters

--- test.pas
+++ test.pas
@@ -1,7 +1,6 @@
 michael was here
 this language is pretty rad
 but also pretty bizarre
-this line is quite long; hopefully longer then the 72 character limit.
-Alas, it required a second sentence.
+this line is quite long; hopefully longer then the 72 character limit. Alas, it required a second sentence.
 howdy
 EOF
