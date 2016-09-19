2cad2f7d621fc18bf212c67c355d397ecb6f1973
Michael Ripley <zkxs00@gmail.com>
2016-09-19 12:15:04 -0500

    Move IntegerTokenizer out of its own class

--- src/net/michaelripley/pascalcompiler/Lexer.scala
+++ src/net/michaelripley/pascalcompiler/Lexer.scala
@@ -1,11 +1,29 @@
 package net.michaelripley.pascalcompiler
 
 import scala.io.Source
+import scala.util.matching.Regex.Match
 
 object Lexer {
   def main(args: Array[String]): Unit = {
     val lexer = new Lexer(Source.fromFile("test.pas"))
     lexer.lex()
+  }
+  
+  val integerToken = new PartialToken("INTEGER", None)
+  val integerErrorTooLong = new PartialToken("LEXERR", Some("Integer too long"))
+  val integerErrorLeadingZero = new PartialToken("LEXERR", Some("Integer has leading zeros"))
+  
+  val integerTokenizer = new SimpleTokenizer("""\d+""".r, integerToken) {
+    override def checkError(matchResult: Match, lexeme: Lexeme): Option[Token] = {
+      val intString = matchResult.matched
+      if (intString.length() > 10) {           // check if integer is too long
+        Some(integerErrorTooLong.makeToken(lexeme))
+      } else if (intString.charAt(0) == '0') { // check if integer has leading zeros
+        Some(integerErrorLeadingZero.makeToken(lexeme))
+      } else {
+        None
+      }
+    }
   }
 }
 
---
+++ src/net/michaelripley/pascalcompiler/SimpleTokenizer.scala
@@ -0,0 +1,45 @@
+package net.michaelripley.pascalcompiler
+
+import scala.util.matching.Regex
+import scala.util.matching.Regex.Match
+
+/**
+ * Simple fill-in-the-blanks Tokenizer implementation that should work for most use cases
+ * @param pattern Pattern that matches the lexeme for this type of token
+ * @param tokenCreator Method to create token if match had no errors (see {@link #checkError})
+ */
+class SimpleTokenizer(val pattern: Regex, val tokenCreator: (Match, Lexeme) => Token) extends Tokenizer {
+  
+  /**
+   * Alternate constructor that is useful for lexemes that always tokenize
+   * to the same exact token.
+   * @param pattern Pattern that matches the lexeme for this type of token
+   * @param partialToken Partial token to add the lexeme to
+   */
+  def this(pattern: Regex, partialToken: PartialToken) = {
+    this(pattern, (m, l) => partialToken.makeToken(l))
+  }
+    
+  final def extractToken(line: String, lineNumber: Int, columnOffset: Int): Option[Token] = {
+    pattern.findFirstMatchIn(line) match {
+      case Some(matchResult) => {                    // there was a match
+        val lexeme = new Lexeme(matchResult.matched, lineNumber, columnOffset) // create the lexeme
+        checkError(matchResult, lexeme) match {              // check for lexical errors in the math (e.g. number too long)
+          case Some(errorToken) => Some(errorToken)  // There was an error. Return its token.
+          case None => {                             // There was no error.
+            Some(tokenCreator(matchResult, lexeme))   // create the token for the match
+          }
+        }
+      }
+      case None => None                              // There was no match :(
+    }
+  }
+  
+  /**
+   * Check a match to see if it is valid. By default, all matches are valid.
+   * @return A token representing the error, or None if there was no error.
+   */
+  def checkError(matchResult: Match, lexeme: Lexeme): Option[Token] = {
+    None
+  }
+}
