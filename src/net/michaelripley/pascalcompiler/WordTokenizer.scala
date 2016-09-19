dcb5faeb66c7c216a104eaabbea2a6942efeb8cb
Michael Ripley <zkxs00@gmail.com>
2016-09-19 12:14:52 -0500

    Made incomplete WordTokenizer

--- src/net/michaelripley/pascalcompiler/WordTokenizer.scala
+++ src/net/michaelripley/pascalcompiler/WordTokenizer.scala
@@ -1,10 +1,24 @@
 package net.michaelripley.pascalcompiler
 
+object WordTokenizer {
+  val pattern = """[a-z][a-z0-9]*""".r
+  val wordToken = new PartialToken("FIXME", None)
+}
+
+import WordTokenizer._
+
 /**
  * Tokenizes both identifers and reserved words
  */
 class WordTokenizer extends Tokenizer {
   def extractToken(line: String, lineNumber: Int, columnOffset: Int): Option[Token] = {
-    None
+    
+    pattern.findFirstIn(line) match {
+      case Some(wordString) => {
+        val lexeme = new Lexeme(wordString, lineNumber, columnOffset)
+        Some(wordToken.makeToken(lexeme))
+      }
+      case None => None
+    }
   }
 }
