05cd5396c28c5b071f6ac86d64ccae087adc2a8d
Michael Ripley <zkxs00@gmail.com>
2016-09-22 01:58:24 -0500

    Document tokenizer

--- src/net/michaelripley/pascalcompiler/tokenizers/Tokenizer.scala
+++ src/net/michaelripley/pascalcompiler/tokenizers/Tokenizer.scala
@@ -5,7 +5,7 @@ import net.michaelripley.pascalcompiler.tokens.Token
 trait Tokenizer {
   
   /**
-   * Extract a token. Will not have leading space.
+   * Extract a token. Will not have leading space, and will not be empty.
    */
   def extractToken(line: String, lineNumber: Int, columnOffset: Int): Option[Token];
 }
