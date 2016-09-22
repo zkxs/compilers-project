b3d1f497c7b99a3ad159a23ff9dcdbf074aa7e5d
Michael Ripley <zkxs00@gmail.com>
2016-09-22 00:10:05 -0500

    Created StringTokenizer

---
+++ src/net/michaelripley/pascalcompiler/tokenizers/StringTokenizer.scala
@@ -0,0 +1,73 @@
+package net.michaelripley.pascalcompiler.tokenizers
+
+private object StringTokenizer {
+  
+  /**
+   * Get the longest string in an iterable. Returns None if it is empty or
+   * if there is a tie for longest.
+   */
+  private def getLongest(list: Iterable[String]): Option[String] = {
+    
+    if (list.isEmpty) {
+      return None
+    }
+    
+		var currentElem:String = null
+    var currentLength = -1
+    var countWithLength = 0
+    
+    list.foreach(
+      elem => {
+        val elemLength = elem.length()
+        
+        if (elemLength > currentLength) {
+          currentElem = elem
+          currentLength = elemLength
+          countWithLength = 1
+        } else if (elemLength == currentLength) {
+          countWithLength += 1
+        }
+      }
+    )
+    
+    /*  because the list is nonempty and the initial value of
+     *  currentLength = -1, it is guaranteed that our three variables
+     *  are valid, eg. currentElem is not null and countWithLength < 0
+     */
+    
+    if (countWithLength == 1) {
+      return Some(currentElem)
+    } else {
+      return None
+    }
+  }
+  
+}
+
+import net.michaelripley.pascalcompiler.tokens._
+import net.michaelripley.pascalcompiler.ReservedStrings
+import StringTokenizer._
+
+class StringTokenizer(private val reservedStrings: ReservedStrings) extends Tokenizer {
+  
+  private val reservedStringKeys = reservedStrings.getStrings()
+  
+  def extractToken(line: String, lineNumber: Int, columnOffset: Int): Option[AttributeToken] = {
+    
+    // get all reserved strings that prefix line
+    val matchingStrings = reservedStringKeys.filter(rString => line.startsWith(rString))
+    
+    // get the longest string, if there is one
+    val longestString = getLongest(matchingStrings)
+    
+    longestString match {
+      case Some(string) => {
+        // if there WAS a longest string, tokenize and return it
+        val lexeme = new Lexeme(string, lineNumber, columnOffset)
+        Some(reservedStrings(string).makeToken(lexeme))
+      }
+      case None => None // if there was no longest string, we couldn't get a token
+    }
+  }
+  
+}
