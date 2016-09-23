0a94eb124cb44e5dffcd19ba7461f64a7db0f868
Michael Ripley <zkxs00@gmail.com>
2016-09-23 02:00:14 -0500

    Made a file that contains all possible errors

---
+++ test2_errors.pas
@@ -0,0 +1,24 @@
+identifier errors
+longidentifier
+
+integer errors
+12345678901
+01
+
+real errors
+00.0
+0.00
+123456.0
+0.123456
+
+longreal errors
+00.0E1
+0.00E1
+0.0E0
+0.0E01
+123456.0E1
+0.123456E1
+0.0E100
+
+garbage character errors
+@#$
