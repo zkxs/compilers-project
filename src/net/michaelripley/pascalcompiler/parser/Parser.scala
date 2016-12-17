0be9e33aa401920836ef4123ac1e15c1a67f0775
Michael Ripley <zkxs00@gmail.com>
2016-12-17 17:51:14 -0600

    Declare a zillion empty functions
    Boy oh boy is this gonna be fun :(

---
+++ src/net/michaelripley/pascalcompiler/parser/Parser.scala
@@ -0,0 +1,158 @@
+package net.michaelripley.pascalcompiler.parser
+
+class Parser {
+  
+  def program() = {
+    
+  }
+  
+  def programPrime() = {
+    
+  }
+  
+  def identifierList() = {
+    
+  }
+  
+  def identifierListTail() = {
+    
+  }
+  
+  def declarations() = {
+    
+  }
+  
+  def optionalDeclarations() = {
+    
+  }
+  
+  // could not name this function type, as type is a reserved word in Scala
+  def anyType() = {
+    
+  }
+  
+  def standardType() = {
+    
+  }
+  
+  def subprogramDeclarations() = {
+    
+  }
+  
+  def optionalSubprogramDeclarations() = {
+    
+  }
+  
+  def subprogramDeclaration() = {
+    
+  }
+  
+  def subprogramDeclarationPrime() = {
+    
+  }
+  
+  def subprogramHead() = {
+    
+  }
+  
+  def subprogramHeadPrime() = {
+    
+  }
+  
+  def arguments() = {
+    
+  }
+  
+  def parameterList() = {
+    
+  }
+  
+  def parameterListTail() = {
+    
+  }
+  
+  def compoundStatement() = {
+    
+  }
+  
+  def compoundStatmentTail() = {
+    
+  }
+  
+  def statementList() = {
+    
+  }
+  
+  def statementListTail() = {
+    
+  }
+  
+  def statement() = {
+    
+  }
+  
+  def optionalElse() = {
+    
+  }
+  
+  def variable() = {
+    
+  }
+  
+  def arrayVariable() = {
+    
+  }
+  
+  def procedureStatement() = {
+    
+  }
+  
+  def optionalExpressionList() = {
+    
+  }
+  
+  def expressionList() = {
+    
+  }
+  
+  def expressionListTail() = {
+    
+  }
+  
+  def expression() = {
+    
+  }
+  
+  def optionalRelop() = {
+    
+  }
+  
+  def simpleExpression() = {
+    
+  }
+  
+  def optionalAddop() = {
+    
+  }
+  
+  def term() = {
+    
+  }
+  
+  def optionalMulop() = {
+    
+  }
+  
+  def factor() = {
+    
+  }
+  
+  def arrayExpression() = {
+    
+  }
+  
+  def sign() = {
+    
+  }
+  
+}
